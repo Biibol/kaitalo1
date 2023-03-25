4aa9796e8a9a9f3783133e234f60f44ea40ef108
biibo <biibolot23@gmail.com>
2023-03-25 21:50:20 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static boolean aBoolean;
+
+    public static void main(String[] args) {
+
+
+               /*  String  n =" I Like Java!!!";
+               System.out.println(n.charAt(n.length()-1));
+               System.out.println(n.endsWith("!!!"));
+               System.out.println(n.startsWith("I Like"));
+               System.out.println(n.contains("I Like"));
+               System.out.println(n.replace("a","o"));
+               System.out.println(n.toUpperCase());
+               System.out.println(n.toLowerCase());
+               System.out.println(n.length());
+               System.out.println(n.trim());*/
+            }
+    }
+
+
+
+
+
+
+
